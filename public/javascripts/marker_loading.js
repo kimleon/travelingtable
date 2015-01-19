3cde566df3cad2b203c39b685405edabe13e8c40
kiranw <kiranw@mit.edu>
2015-01-18 22:38:31 -0500

    errors in undefined return from ajax?

--- public/javascripts/marker_loading.js
+++ public/javascripts/marker_loading.js
@@ -1,13 +1,8 @@
 //add function call for dragend event and refresh every 5s, resizing
 	var locations = []; //A repository for markers (and the data from which they were contructed).//var edges = null;
-//this will contain the markers loaded within map edges? idk initialize this with existing db maybe?
-
-//new markers that are scanned and passed in
-	//initial dataset for markers
-	//var data.len = 0;
 
 
-	function refreshMap() {
+	var refreshMap = function(map) {
 		console.log('function refreshmap called')
 	// get edges
 	var edges = map.getBounds();
@@ -15,9 +10,6 @@
 	var top_coord = edges.getSouthWest().lng();
 	var right_coord = edges.getNorthEast().lat();
 	var bottom_coord = edges.getNorthEast().lng();
-
-
-	//ajax get request - length
 
 
 	//ajax post edges
@@ -33,94 +25,22 @@
 
 
 
-
-
-
-	//compare lengths, if length_Received > len, ajax get request of last length_Received-len values within bounds
-//map info ya know the usual
-
-	/*var map = new google.maps.Map(document.getElementById('#map'), {
-	    zoom: 1,
-	    maxZoom: 8,
-	    minZoom: 1,
-	    streetViewControl: false,
-	    center: new google.maps.LatLng(30, 30),
-	    mapTypeId: google.maps.MapTypeId.ROADMAP
-	});*/
-
-
-
 	//iterate through them if within bounds, and not already in locations (already loaded) markers, then display
 	//append to locations
-	function setMarkers(locObj) {
+	var setMarkers = function(locObj) {
 	    $.each(locObj, function (loc) {
-	        //if (!locations[loc[0]] && loc[1] !== undefined && loc[2] !== undefined) {
-			//if ($.inArray(loc[0],locations)!== -1 && loc[1] !== undefined && loc[2] !== undefined) {
-	        	//$.inArray(value, array)
-	            //Marker has not yet been made (and there's enough data to create one).
-	            //ALSO ADD AN EXTRA LINE FOR IF IT IS WITHIN OUR BOUNDS, if lat in range(bounds), and lng in range(bounds)
-	            //Create marker
+	        
 	            loc.marker = new google.maps.Marker({
 	                position: new google.maps.LatLng(loc[1], loc[2]),
 	                map: map
 	            });
-	            //what is this click listener business, oh crap this displays the usernames okay
-	            //Attach click listener to marker
-	            /*google.maps.event.addListener(loc.marker, 'click', (function (key) {
-	                return function () {
-	                    infowindow.setContent(locations[key].info);
-	                    infowindow.open(map, locations[key].marker);
-	                }
-	            })(key));*/
+	           
 	            //Remember loc in the `locations` so its info can be displayed and so its marker can be deleted.
 	            locations.push(loc[0]);
-	        } /*else if (locations[key] && loc.remove) {
-	            //Remove marker from map
-	            if (locations[key].marker) {
-	                locations[key].marker.setMap(null);
-	            }
-	            //Remove element from `locations`
-	            delete locations[key];
-	        } else if (locations[key]) {
-	            //Update the previous data object with the latest data.
-	            $.extend(locations[key], loc);
-	            if (loc.lat !== undefined && loc.lng !== undefined) {
-	                //Update marker position (maybe not necessary but doesn't hurt).
-	                locations[key].marker.setPosition(
-	                new google.maps.LatLng(loc.lat, loc.lng));
-	            }
-	            //locations[key].info looks after itself.
-	        }*/
-	    });
-	}
-/*
-	var ajaxObj = { //Object to save cluttering the namespace.
-	    options: {
-	        url: "........", //The resource that delivers loc data.
-	        dataType: "json" //The type of data tp be returned by the server.
-	    },
-	    delay: 10000, //(milliseconds) the interval between successive gets.
-	    errorCount: 0, //running total of ajax errors.
-	    errorThreshold: 5, //the number of ajax errors beyond which the get cycle should cease.
-	    ticker: null, //setTimeout reference - allows the get cycle to be cancelled with clearTimeout(ajaxObj.ticker);
-	    get: function () { //a function which initiates 
-	        if (ajaxObj.errorCount < ajaxObj.errorThreshold) {
-	            ajaxObj.ticker = setTimeout(getMarkerData, ajaxObj.delay);
-	        }
-	    },
-	    fail: function (jqXHR, textStatus, errorThrown) {
-	        console.log(errorThrown);
-	        ajaxObj.errorCount++;
-	    }
-	};
-	//Ajax master routine
-	function getMarkerData() {
-	    $.ajax(ajaxObj.options)
-	        .done(setMarkers) //fires when ajax returns successfully
-	    .fail(ajaxObj.fail) //fires when an ajax error occurs
-	    .always(ajaxObj.get); //fires after ajax success or ajax error
-	}
-*/
+	        
+	 });
+	 }
+
 	setMarkers(new_locations); //Create markers from the initial dataset served with the document.
 	//ajaxObj.get(); //Start the get cycle.
 }
