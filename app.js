f1316e5e1c597f66134e7ba0d1f97642eae2e08a
Deepti Raghavan <deeptir@mit.edu>
2015-01-24 16:40:38 -0500

    re adding thing after it has been deployed

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ var fs = require('fs');
 
 
 
-mongoose.connect('mongodb://heroku_app33378596:fe1jgskkc0aijfs9lscj4kdgr4@ds031551.mongolab.com:31551/heroku_app33378596');
+//mongoose.connect('mongodb://heroku_app33378596:fe1jgskkc0aijfs9lscj4kdgr4@ds031551.mongolab.com:31551/heroku_app33378596');
 
-//mongoose.connect('mongodb://localhost/test');
+mongoose.connect('mongodb://localhost/test');
 
 
 // //require the AWS module
